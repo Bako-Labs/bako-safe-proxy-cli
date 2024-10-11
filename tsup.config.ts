56f8090fb25fe37d20205fc7ee153b9316c74ccd
luisburigo <guga.burigo29@outlook.com>
2024-10-10 22:20:43 -0300

    chore: add shebang on build

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,8 +5,11 @@ export default {
   format: ['cjs'], // Provide CommonJS and ESM support for wider compatibility
   minify: true, // Usually, you want to minify the CLI for performance
   entry: ['./src/index.ts'], // Entry point remains the same unless specified otherwise
-  target: 'node14', // Target a specific Node.js version for CLI
+  target: 'node22', // Target a specific Node.js version for CLI
   outDir: 'dist', // Specify output directory for clarity
   clean: true, // Clean the output directory before building
   noExternal: [],
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
 };
